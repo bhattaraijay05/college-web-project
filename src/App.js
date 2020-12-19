b19e7f81ab598171e1941f0748adbefabec49ead
bhattaraijay05 <54356944+bhattaraijay05@users.noreply.github.com>
2020-12-19 17:42:15 +0530

    authenticationadded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Authentication/Login";
+import Signup from "./components/Authentication/Signup";
 import About from "./components/Homepage/About";
 import Home from "./components/Homepage/Home";
 import Main from "./components/Homepage/Main";
@@ -9,12 +11,24 @@ import P404 from "./components/P404";
 import Footer from "./components/shared/Footer";
 import NavBar from "./components/shared/NavBar";
 import AddToFirestore from "./Firebase/AddToFirestore";
+import { auth } from "./Firebase/Firebase";
 
 const NoMatchPage = () => {
   return <P404 />;
 };
 
 function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [user]);
   return (
     <div className="mainbody">
       <NavBar />
@@ -22,6 +36,8 @@ function App() {
         <Route path="/" component={Main} exact />
         <Route path="/about" component={About} exact />
         <Route path="/our-team" component={TeamPage} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/signup" component={Signup} exact />
         <Route path="/add" component={AddToFirestore} exact />
         <Route path={`/:book/:chapters/:shloka`} component={Home} exact />
         <Route path={`/:book/:chapters`} component={Home} exact />
--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,38 +1,56 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../Firebase/Firebase";
+
 const NavBar = () => {
+  console.log(auth.currentUser);
   return (
-    <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <Link class="navbar-brand" to="/">
-          Super Site
-        </Link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <Link class="nav-link" to="/" style={{ color: "white" }}>
-              Home
-            </Link>
-            <Link class="nav-link" to="/about" style={{ color: "white" }}>
-              About Page
-            </Link>
-            <Link class="nav-link" to="/our-team" style={{ color: "white" }}>
-              Our Team
-            </Link>
-          </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
+      <Link className="navbar-brand" to="/">
+        Super Site
+      </Link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav  mr-auto">
+          <Link className="nav-link" to="/" style={{ color: "white" }}>
+            Home
+          </Link>
+          <Link className="nav-link" to="/about" style={{ color: "white" }}>
+            About Page
+          </Link>
+          <Link className="nav-link" to="/our-team" style={{ color: "white" }}>
+            Our Team
+          </Link>
         </div>
-      </nav>
-    </>
+        <div class=" my-2 my-lg-0">
+          {auth.currentUser ? (
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <h4 style={{ color: "#fff", marginRight: 70 }}>
+                Hi {auth.currentUser.displayName}
+              </h4>
+              <Button onClick={() => auth.signOut()} style={{ color: "red" }}>
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <Link className="nav-link " to="/login" style={{ color: "white" }}>
+              Login/Signup
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 };
 
